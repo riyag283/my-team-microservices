9ec4a715fa5b1310b7cb8539178fb150467bf33d
Riya Gupta <riya.gupta@publicissapient.com>
2023-02-20 17:45:02 +0530

    Revert "testify and go mock"
    This reverts commit adbd0a25f97a385abdceede79573f7d571d09a43.

--- teams-service/db/db_client.go
+++ teams-service/db/db_client.go
@@ -1,7 +1,6 @@
 package db
 
 import (
-	"database/sql"
 	"log"
 	util "teams/utils"
 
@@ -9,51 +8,22 @@ import (
 	_ "github.com/lib/pq"
 )
 
-type DBClientInterface interface {
-    Ping() error
-    Close() error
-    Exec(query string, args ...interface{}) (sql.Result, error)
-    Get(dest interface{}, query string, args ...interface{}) error
-    Select(dest interface{}, query string, args ...interface{}) error
-    QueryRow(query string, args ...interface{}) *sql.Row
-    QueryRowx(query string, args ...interface{}) *sqlx.Row
-    Query(query string, args ...interface{}) (*sql.Rows, error)
-}
-
-type DBClient struct {
-    *sqlx.DB
-}
-
-func (c *DBClient) QueryRow(query string, args ...interface{}) *sql.Row {
-    return c.DB.QueryRow(query, args...)
-}
-
-func (c *DBClient) QueryRowx(query string, args ...interface{}) *sqlx.Row {
-    return c.DB.QueryRowx(query, args...)
-}
-
-func (c *DBClient) Query(query string, args ...interface{}) (*sql.Rows, error) {
-    return c.DB.Query(query, args...)
-}
-
-var (
-    DBClientInstance DBClientInterface
-)
+var DBClient *sqlx.DB
 
 func InitialiseDBConnection() {
-    config, err := util.LoadConfig(".")
-    if err != nil {
-        log.Fatal("cannot load configurations:", err)
-    }
+	config, err := util.LoadConfig(".")
+	if err != nil {
+		log.Fatal("cannot load configurations:", err)
+	}
 
-    db, err := sqlx.Open(config.DBDriver, config.DBSource)
-    if err != nil {
-        panic(err.Error())
-    }
-    err = db.Ping()
-    if err != nil {
-        panic(err.Error())
-    }
+	db, err := sqlx.Open(config.DBDriver, config.DBSource)
+	if err != nil {
+		panic(err.Error())
+	} 
+	err = db.Ping()
+	if err != nil {
+		panic(err.Error())
+	}
 
-    DBClientInstance = &DBClient{db}
+	DBClient = db 
 }
